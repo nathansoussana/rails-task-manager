9bcf14c6786fcdbd55168bc3c6907ab4e75dc95d
Nathan Soussana <nathan.soussana@gmail.com>
2023-05-20 12:07:10 +0200

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "tasks#index"
   get "tasks", to: "tasks#index", as: :tasks
   get "tasks/new", to: "tasks#new", as: :new
   get "tasks/:id", to: "tasks#show", as: :task
